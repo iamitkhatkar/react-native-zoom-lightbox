943b601c505d296234f7db970a54f2972e36bc2f
Amit khatkar <jkhatkar61@gmail.com>
2021-03-08 19:07:29 +0530

    add image caching

--- src/ImageCustom/ImageCustom.js
+++ src/ImageCustom/ImageCustom.js
@@ -1,40 +1,35 @@
 import React, { PureComponent } from 'react';
-import {
-  Image,View,
-} from 'react-native';
+import { View } from 'react-native';
+import Image from 'react-native-fast-image';
 import PropTypes from 'prop-types';
 
 export default class ImageCustom extends PureComponent {
+	static propTypes = {
+	  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+	  index: PropTypes.number,
+	  url: PropTypes.string,
+	};
 
-  static propTypes = {
-    style: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.array,
-    ]),
-    index: PropTypes.number,
-    url:PropTypes.string,
-  }
+	constructor(props) {
+	  super(props);
+	  this.carouselItems = {};
+	}
+	captureCarouselItem = (ref, idx) => {
+	  this.carouselItems[idx] = ref;
+	};
 
-  constructor(props) {
-    super(props);
-    this.carouselItems = {};
-  }
-    captureCarouselItem = (ref, idx) => {
-      this.carouselItems[idx] = ref;
-    }
-
-    render() {
-      const { url, style, index } = this.props;
-      return (
-        <View>
-          <Image
-            ref={ref => this.captureCarouselItem(ref, index)}
-            source={{
-              uri:url,
-            }}
-            style={style}
-          />
-        </View>
-      );
-    }
+	render() {
+	  const { url, style, index } = this.props;
+	  return (
+	    <View>
+	      <Image
+	        ref={(ref) => this.captureCarouselItem(ref, index)}
+	        source={{
+	          uri: url,
+	        }}
+	        style={style}
+	      />
+	    </View>
+	  );
+	}
 }
